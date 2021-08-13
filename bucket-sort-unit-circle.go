5698b41bc4d6301ef353096e9cae7efdb7850f5d
Nelly Kuznetsova <nelly@fwd.be>
2021-08-13 16:13:12 +0200

    Finish bucket sort

--- bucket-sort-unit-circle.go
+++ bucket-sort-unit-circle.go
@@ -2,38 +2,68 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"time"
 	"math/rand"
+	"math"
 )
 
-func sort(distances []float64) {
-	buckets := make([][]float64, len(distances)*n)
-	n := float64(len(distances))
-	for _, d := range distances {
-		//idx := int(d * n) // bad
-		idx := int(d*d*n)
-		buckets[idx] = append(buckets[idx], d)
+func distance(p []float64) float64 {
+	return p[0]*p[0] + p[1]*p[1]
+}
+
+func insertionSort(points [][]float64) {
+	for i := 1; i < len(points); i++ {
+		for j := i; j > 0 && distance(points[i-1]) > distance(points[i]); j-- {
+			points[i-1], points[i] = points[i], points[i-1]
+		}
 	}
-	sizes := make(map[int]int)
-	for _, b := range buckets {
-		sizes[len(b)]++
+}
+
+func sort(points [][]float64) {
+	buckets := make([][][]float64, len(points))
+	n := float64(len(points))
+	for _, p := range points {
+		r := p[0]*p[0] + p[1]*p[1] // radius squared
+		idx := int(r*n) // floor
+		buckets[idx] = append(buckets[idx], p)
 	}
-	fmt.Println("sizes", sizes)
+        /*  test bucket sizes
+		sizes := make(map[int]int)
+		for _, b := range buckets {
+			sizes[len(b)]++
+		}
+		fmt.Println("sizes", sizes)
+	*/
+	// sort each bucket with insertion sort
+	for _, bucket := range buckets {
+		insertionSort(bucket)		
+	}
+	i := 0
+	for _, bucket := range buckets {
+		for _, p := range bucket {
+			points[i] = p
+			i++
+		}	
+	}
 }
 
 func run() {
 	fmt.Println("go!")
 	rand.Seed(time.Now().UnixNano())
-	points := 10000
-	distances := make([]float64, 0)
-	for i := 0; i < points; i++ {
-		x := rand.Float32()
-		y := rand.Float32()
-		r := math.Sqrt(float64(x*x) + float64(y*y))
-		distances = append(distances, r)
+	n := 10
+	points := make([][]float64, 0)
+	for i := 0; i < n; {
+		x := rand.Float64()
+		y := rand.Float64()
+		if math.Sqrt(x*x + y*y) <= 1.0 {
+			points = append(points, []float64{x, y})
+			i++
+		}
 	}
-	sort(distances) 
+	sort(points) 
+	for _, p := range points {
+		fmt.Printf("x: %.5f, y: %.5f r: %.5f\n", p[0], p[1], math.Sqrt(p[0]*p[0] + p[1]*p[1]))
+	}
 }
 
 func main() {
